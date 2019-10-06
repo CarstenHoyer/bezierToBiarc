567e534f3ac34fd418eeda9df1f511ca413f09a1
Carsten Høyer <carsten@reload.dk>
2019-10-06 07:20:05 +0200

    Add cubicBezierToBiarc test

--- assembly/__tests__/cubicBezierToBiarc.spec.ts
+++ assembly/__tests__/cubicBezierToBiarc.spec.ts
@@ -1,7 +1,8 @@
 import { cubicBezierToBiarc } from '../cubicBezierToBiarc'
+import { console } from '../console'
 
 describe("cubicBezierToBiarc", () => {
-  it("calculate biarcs", () => {
+  it("calculates biarcs", () => {
     const points = new Float64Array(8);
     points[0] = 100;
     points[1] = 500;
@@ -15,32 +16,32 @@ describe("cubicBezierToBiarc", () => {
     
     expect<i32>(biarcs.length).toBe(16);
 
-    // expect<f32>(biarcs[0].A1.startAngle).toBeCloseTo(-3.01724);
-    // expect<f32>(biarcs[0].A1.sweepAngle).toBeCloseTo(0.0446029);
-    // expect<f32>(biarcs[0].A1.C.x).toBeCloseTo(763.166);
-    // expect<f32>(biarcs[0].A1.C.y).toBeCloseTo(582.896);
-    // expect<f32>(biarcs[0].A1.r).toBeCloseTo(668.327);
-    // expect<f32>(biarcs[0].A1.cw).toBeCloseTo(1);
+    expect<f64>(biarcs[0].A1.startAngle).toBeCloseTo(0.5205896);
+    expect<f64>(biarcs[0].A1.sweepAngle).toBeCloseTo(0.0649772);
+    expect<f64>(biarcs[0].A1.C.x).toBeCloseTo(170.15441);
+    expect<f64>(biarcs[0].A1.C.y).toBeCloseTo(210.87502);
+    expect<f64>(biarcs[0].A1.r).toBeCloseTo(227.253149);
+    expect<f64>(biarcs[0].A1.cw ? 1 : 0).toBeCloseTo(1);
 
-    // expect<f32>(biarcs[0].A2.startAngle).toBeCloseTo(-2.97264);
-    // expect<f32>(biarcs[0].A2.sweepAngle).toBeCloseTo(0.0474563);
-    // expect<f32>(biarcs[0].A2.C.x).toBeCloseTo(686.351);
-    // expect<f32>(biarcs[0].A2.C.y).toBeCloseTo(569.792);
-    // expect<f32>(biarcs[0].A2.r).toBeCloseTo(590.402);
-    // expect<f32>(biarcs[0].A2.cw).toBeCloseTo(1);
+    expect<f64>(biarcs[0].A2.startAngle).toBeCloseTo(0.58556);
+    expect<f64>(biarcs[0].A2.sweepAngle).toBeCloseTo(0.057934);
+    expect<f64>(biarcs[0].A2.C.x).toBeCloseTo(121.32324);
+    expect<f64>(biarcs[0].A2.C.y).toBeCloseTo(178.49243);
+    expect<f64>(biarcs[0].A2.r).toBeCloseTo(285.845949);
+    expect<f64>(biarcs[0].A2.cw ? 1 : 0).toBeCloseTo(1);
     
-    // expect<f32>(biarcs[15].A1.startAngle).toBeCloseTo(0.520591);
-    // expect<f32>(biarcs[15].A1.sweepAngle).toBeCloseTo(0.064977);
-    // expect<f32>(biarcs[15].A1.C.x).toBeCloseTo(170.152);
-    // expect<f32>(biarcs[15].A1.C.y).toBeCloseTo(210.873);
-    // expect<f32>(biarcs[15].A1.r).toBeCloseTo(227.256);
-    // expect<f32>(biarcs[15].A1.cw).toBeCloseTo(1);
+    expect<f64>(biarcs[15].A1.startAngle).toBeCloseTo(-3.01723);
+    expect<f64>(biarcs[15].A1.sweepAngle).toBeCloseTo(0.04460);
+    expect<f64>(biarcs[15].A1.C.x).toBeCloseTo(763.165415);
+    expect<f64>(biarcs[15].A1.C.y).toBeCloseTo(582.89567);
+    expect<f64>(biarcs[15].A1.r).toBeCloseTo(668.32631);
+    expect<f64>(biarcs[15].A1.cw ? 1: 0).toBeCloseTo(1);
 
-    // expect<f32>(biarcs[15].A2.startAngle).toBeCloseTo(0.585567);
-    // expect<f32>(biarcs[15].A2.sweepAngle).toBeCloseTo(0.0579328);
-    // expect<f32>(biarcs[15].A2.C.x).toBeCloseTo(121.32);
-    // expect<f32>(biarcs[15].A2.C.y).toBeCloseTo(178.491);
-    // expect<f32>(biarcs[15].A2.r).toBeCloseTo(285.85);
-    // expect<f32>(biarcs[15].A2.cw).toBeCloseTo(1);
+    expect<f64>(biarcs[15].A2.startAngle).toBeCloseTo(-2.97263);
+    expect<f64>(biarcs[15].A2.sweepAngle).toBeCloseTo(0.047456);
+    expect<f64>(biarcs[15].A2.C.x).toBeCloseTo(686.34852);
+    expect<f64>(biarcs[15].A2.C.y).toBeCloseTo(569.79191);
+    expect<f64>(biarcs[15].A2.r).toBeCloseTo(590.39978);
+    expect<f64>(biarcs[15].A2.cw ? 1 : 0).toBeCloseTo(1);
   });
 });
